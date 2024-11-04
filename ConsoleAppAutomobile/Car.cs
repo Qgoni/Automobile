39807901d298e6abf459c9146fcda3bd8f82cec4
qgoni <pasword20.securety@gmail.com>
2024-11-04 22:21:33 +0300

    Add method GetClassAuto now abstract

--- ConsoleAppAutomobile/Car.cs
+++ ConsoleAppAutomobile/Car.cs
@@ -6,27 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppAutomobile
 {
-    public class Car
+    public abstract class Car
     {
         public int EnginePower { get; set; }
         public string model { get; set; }
-        public int VINid { get; set; }
+        public long VINid { get; set; }
         public string Color { get; set; }
 
         /// <summary>
         /// Возвращает максимальную скорость автомобиля в зависимости от модели передаваемой в метод.
         /// </summary>
         /// <returns>Максимальная скорость автомобиля в км/ч и ее модель</returns>
-        public virtual void ReturnMaxSpeed(string model)
-        {
-            if (model == null)
-            {
-                Console.WriteLine($"Не определена");
-            }
-            else
-            {
-                Console.WriteLine($"Модель {model} скорость...");
-            }
-        }
+        public abstract void ReturnMaxSpeed();
+        public abstract void GetClassAuto();
     }
 }
