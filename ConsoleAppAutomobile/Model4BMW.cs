ccdeba0003d0efac6064662b67bd3b3000fcbe9e
qgoni <pasword20.securety@gmail.com>
2024-11-04 22:34:20 +0300

    added the BMW Model 4 class with redefinition of methods

---
+++ ConsoleAppAutomobile/Model4BMW.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAutomobile
+{
+    internal class Model4BMW : BMW
+    {
+        public override void GetClassAuto()
+        {
+            Console.WriteLine($"Модель 4 - D-класс");
+        }
+
+        public override void GetTheTypeOfCar()
+        {
+            Console.WriteLine($"Модель 4 - не спортивная");
+        }
+
+        public override void ReturnMaxSpeed()
+        {
+            Console.WriteLine($"Модель 4 скорость 250 км/ч");
+        }
+    }
+}
