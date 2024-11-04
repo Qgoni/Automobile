64e694eb818f606e008ea414d3ba120c7e241270
qgoni <pasword20.securety@gmail.com>
2024-11-04 22:28:48 +0300

    the class has become abstract, methods have been added, the type of machine and the list of models

--- ConsoleAppAutomobile/BMW.cs
+++ ConsoleAppAutomobile/BMW.cs
@@ -6,43 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppAutomobile
 {
-    public class BMW : Car
+    public abstract class BMW : Car
     {
-        /// <summary>
-        /// Возвращает максимальную скорость автомобиля в зависимости от модели передаваемой в метод.
-        /// <para> Допустимые модели для BMW: 2, 3, 4, M4, 5, M5 </para>
-        /// </summary>
-        /// <returns>Максимальная скорость автомобиля в км/ч и ее модель</returns>
-        
-        public override void ReturnMaxSpeed(string model)
+        public abstract void GetTheTypeOfCar();
+        public void GetListModelCar()
         {
-            if (model == "2")
+            Console.WriteLine("Список моделей BMW:");
+            string[] arrayCar = {
+                "BMW 2 серии",
+                "BMW 3 серии",
+                "BMW 4 серии",
+                "BMW M4 серии",
+                "BMW 5 серии",
+                "BMW M5 серии"
+            };
+
+            foreach (string car in arrayCar) 
             {
-                Console.WriteLine($"Модель {model} скорость 250 км/ч");
-            }
-            if (model == "3")
-            {
-                Console.WriteLine($"Модель {model} скорость 250 км/ч");
-            }
-            if (model == "4")
-            {
-                Console.WriteLine($"Модель {model} скорость 250 км/ч");
-            }
-            if (model == "M4")
-            {
-                Console.WriteLine($"Модель {model} скорость 280 км/ч");
-            }
-            if (model == "5")
-            {
-                Console.WriteLine($"Модель {model} скорость 237 км/ч");
-            }
-            if (model == "M5")
-            {
-                Console.WriteLine($"Модель {model} скорость 305 км/ч");
-            }
-            else
-            {
-                Console.WriteLine("Скорость не определена");
+                Console.WriteLine(car);
             }
         }
     }
