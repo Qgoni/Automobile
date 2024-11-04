e806c542cc619bde7672d45dbc5e9111b86ea0de
qgoni <pasword20.securety@gmail.com>
2024-11-04 22:35:57 +0300

    added the BMW Model 5 class with redefinition of methods

---
+++ ConsoleAppAutomobile/Model5BMW.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAutomobile
+{
+    internal class Model5BMW : BMW
+    {
+        public override void GetClassAuto()
+        {
+            Console.WriteLine($"Модель 5 - D-класс");
+        }
+
+        public override void GetTheTypeOfCar()
+        {
+            Console.WriteLine($"Модель 5 - не спортивная");
+        }
+
+        public override void ReturnMaxSpeed()
+        {
+            Console.WriteLine($"Модель 5 скорость 237 км/ч");
+        }
+    }
+}
