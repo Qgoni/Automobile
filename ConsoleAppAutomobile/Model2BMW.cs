58162c76bb703ef30b653dd28142ef1c229fa095
qgoni <pasword20.securety@gmail.com>
2024-11-04 22:31:28 +0300

    added the BMW Model 2 class with redefinition of methods

---
+++ ConsoleAppAutomobile/Model2BMW.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAutomobile
+{
+    internal class Model2BMW : BMW
+    {
+        public override void GetClassAuto()
+        {
+            Console.WriteLine($"Модель 2 - C-класс");
+        }
+
+        public override void GetTheTypeOfCar()
+        {
+            Console.WriteLine($"Модель 2 - не спортивная");
+        }
+
+        public override void ReturnMaxSpeed()
+        {
+            Console.WriteLine($"Модель 2 скорость 250 км/ч");
+        }
+    }
+}
