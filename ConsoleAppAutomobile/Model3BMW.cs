9f55b0e773412f9d266d132c2563cd1a43235473
qgoni <pasword20.securety@gmail.com>
2024-11-04 22:33:19 +0300

    added the BMW Model 3 class with redefinition of methods

---
+++ ConsoleAppAutomobile/Model3BMW.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAutomobile
+{
+    internal class Model3BMW : BMW
+    {
+        public override void GetClassAuto()
+        {
+            Console.WriteLine($"Модель 3 - D-класс");
+        }
+
+        public override void GetTheTypeOfCar()
+        {
+            Console.WriteLine($"Модель 3 - не спортивная");
+        }
+
+        public override void ReturnMaxSpeed()
+        {
+            Console.WriteLine($"Модель 3 скорость 250 км/ч");
+        }
+    }
+}
