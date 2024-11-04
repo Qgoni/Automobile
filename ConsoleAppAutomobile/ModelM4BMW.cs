b582d067977c920b57f8defb72d8d7c06dfad114
qgoni <pasword20.securety@gmail.com>
2024-11-04 22:37:22 +0300

    added the BMW Model M4 class with redefinition of methods

---
+++ ConsoleAppAutomobile/ModelM4BMW.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAutomobile
+{
+    internal class ModelM4BMW : BMW
+    {
+        public override void GetClassAuto()
+        {
+            Console.WriteLine($"Модель M4 - D-класс");
+        }
+
+        public override void GetTheTypeOfCar()
+        {
+            Console.WriteLine($"Модель M4 - спорткар");
+        }
+
+        public override void ReturnMaxSpeed()
+        {
+            Console.WriteLine($"Модель M4 скорость 280 км/ч");
+        }
+    }
+}
