a8476cc089ded6c3ddce7a44052a08d60968f371
qgoni <pasword20.securety@gmail.com>
2024-11-13 20:13:55 +0300

    add GetBrand

--- ConsoleAppAutomobile/Car.cs
+++ ConsoleAppAutomobile/Car.cs
@@ -17,6 +17,7 @@ namespace ConsoleAppAutomobile
         /// Возвращает максимальную скорость автомобиля в зависимости от модели передаваемой в метод.
         /// </summary>
         /// <returns>Максимальная скорость автомобиля в км/ч и ее модель</returns>
+        public abstract void GetTheTypeOfCar();
         public abstract void GetBrand();
         public abstract void ReturnMaxSpeed();
         public abstract void GetClassAuto();
